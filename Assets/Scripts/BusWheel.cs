336fc577e89b638da2518baa77058d17295ca4b2
be9904 <be9904@gmail.com>
2022-07-17 02:48:28 +0900

    Added Intro

---
+++ Assets/Scripts/BusWheel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusWheel : MonoBehaviour
+{
+    public float Speed = 200;
+    public bool IsSpinning;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsSpinning)
+            transform.Rotate(Speed * Time.deltaTime, 0, 0);
+    }
+}
