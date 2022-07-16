de947eae0308159f1db4c074b095d26f88397c0a
be9904 <be9904@gmail.com>
2022-07-17 06:07:59 +0900

    Added ResetCam Function

--- Assets/Scripts/SpawnPlayer.cs
+++ Assets/Scripts/SpawnPlayer.cs
@@ -9,6 +9,7 @@ public class SpawnPlayer : MonoBehaviour
     public Transform SpawnPoint;
 
     public CinemachineVirtualCamera vcam01;
+    bool camIsReset;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,20 @@ public class SpawnPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Road.playerSpawned)
+        if(Road.playerSpawned && !camIsReset)
         {
             PlayerControl player = FindObjectOfType<PlayerControl>();
             vcam01.m_LookAt = player.gameObject.transform;
             vcam01.m_Follow = player.gameObject.transform;
+            StartCoroutine(WaitAndReset());
         }
+    }
+
+    IEnumerator WaitAndReset()
+    {
+        yield return new WaitForSeconds(1f);
+        vcam01.m_LookAt = null;
+        vcam01.m_Follow = null;
+        camIsReset = true;
     }
 }
