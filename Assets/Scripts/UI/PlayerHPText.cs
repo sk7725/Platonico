3c5d0083d506c98d691976f88b270cfafc5d1707
Sunny Kim <starwin@gmail.com>
2022-07-17 12:42:16 +0900

    hp

---
+++ Assets/Scripts/UI/PlayerHPText.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPText : MonoBehaviour
+{
+    public string[] hakjum;
+    public Text text;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
